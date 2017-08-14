6d65ea9b155e467e1731a08f6c03732a8876b649
ditero <anelesigenu@gmail.com>
2017-08-14 17:08:45 +0200

    Retrieve names from mongo and update counter

--- models.js
+++ models.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 //const MongoClient = require('mongodb').MongoClient, format = require('util').format;
 module.exports = function(mongoURL){
+  mongoose.Promise = global.Promise;
   mongoose.connect(mongoURL);
 
   const UsersSchema = mongoose.Schema({name : String});
--- myusers.js
+++ myusers.js
@@ -1,100 +1,141 @@
 'use strict'
-module.exports = function(models){
+module.exports = function(models) {
 
-///////////GETUSER FUNCTION: RETUNRS THE VARIABLE <USER>/////////
-  function getUser(req, res){
+  ///////////GETUSER FUNCTION: RETUNRS THE VARIABLE <USER>/////////
+  function getUser(req, res) {
 
-  //  var user = req.params.user;
-  var user = req.body.user;
+    //  var user = req.params.user;
+    var user = req.body.user;
     return user;
   }
 
-var greetedUsers = [];
-var uniqueList = [];
-var countGreeted = 0;
+  var greetedUsers = [];
+  //var uniqueList = [];
+  var countGreeted;
 
-const index = function(req, res){
-  var countGreeted = uniqueList.length;
-  res.render('myusers/index', {countGreeted: countGreeted});
-};
-var convertText = function(name){
-    var userName = name.substring(0,1).toUpperCase() +""+name.substring(1).toLowerCase()
+  const index = function(req, res) {
+    res.render('myusers/index');
+  };
+  var convertText = function(name) {
+    var userName = name.substring(0, 1).toUpperCase() + "" + name.substring(1).toLowerCase()
     return userName;
-};
-/////////////////////GREET SCREEN PAGE////////////////////////////
-const greetScreen = function(req, res){
-  res.render("myusers/greet");
-
-};
-
-//////GREET FUNCTION: RESPONDS AND PUSHES NAMES TO AN ARRAY///////
-  const greet = function(req, res, next){
-  var user = {
-    name: req.body.user
   };
 
-  var selectedRadio = req.body.selectedRadio;
-  var myChoice = selectedRadio + ', '+user.name;
-  var foundUser = uniqueList.find(function(currentUser){
-    return currentUser === user.name;
-  });
-  if (!user || selectedRadio === undefined) {
-    req.flash('error', 'Input field required');
-    res.redirect('/');
+  /////////////////////////Generate names Only from the Database collection/////////////////////////
+  var getNames = function(collection) {
+    //  findNames();
+    var computedNames = [];
+    for (var i = 0; i < collection.length; i++) {
+      computedNames.push(collection[i].name);
+    }
+    //uniqueList = computedNames;
+    return computedNames;
+  };
 
-  }
-  else {
-        models.Users.create(user, function(err, results){
-          if (err) {
-            if (err.code === 11000) {
-              // req.flash('error', 'Welcome back');
-            }
-            else {
-              return next(err);
-            }
+  ////////////////////////////FIND NAMES FROM MONGOOSE DATABASE//////////////////////////////////
+  var findCollection = function(cb) {
+    models.Users.find({}).exec(function(err, results){
+      if (err){
+        return cb(err);
+      }
+      var names = getNames(results);
+      cb(null, names);
+    });
+  };
+
+  /////////////////////GREET SCREEN PAGE////////////////////////////
+  const greetScreen = function(req, res) {
+    findCollection(function(err, results){
+      res.render("myusers/greet", {
+        countGreeted: results.length
+      });
+    });
+  };
+
+  //////GREET FUNCTION: RESPONDS AND PUSHES NAMES TO AN ARRAY///////
+  const greet = function(req, res, next) {
+    var user = {
+      name: req.body.user
+    };
+
+    var selectedRadio = req.body.selectedRadio;
+    var myChoice = selectedRadio + ', ' + user.name;
+
+
+    if (!user.name || selectedRadio === undefined) {
+      req.flash('error', 'Input field required');
+      res.redirect('/');
+
+    } else {
+
+      models.Users.create(user, function(err, results) {
+        if (err) {
+          if (err.code === 11000) {
+            // req.flash('error', 'Welcome back');
+          } else {
+            return next(err);
           }
-        });
-        if (!foundUser) {
-          uniqueList.push(user.name);
         }
-  }
+        else {
+          findCollection(function(err, enteredNames) {
+            if (err) {
+              return next(err);
+            } else {
+              res.render('myusers/greet', {
+                output: myChoice,
+                countGreeted: enteredNames.length
+              });
+            }
+          });
+        }
+      });
 
-  countGreeted = uniqueList.length;
-  greetedUsers.push(user.name);
-    res.render('myusers/greet', {output: myChoice, countGreeted: countGreeted});
+      // if (!foundUser) {
+      //   uniqueList.push(user.name);
+      // }
+
+    }
+    console.log(user.name);
+
+    //greetedUsers.push(user.name);
 
   } //End Of Greet Function
 
-/////////////COUNTGREETINGS FUNCTION: COUNTS HOW TIMES A USER HAS BEEN GREETED///////
-  const countGreetings = function(req, res){
+  /////////////COUNTGREETINGS FUNCTION: COUNTS HOW TIMES A USER HAS BEEN GREETED///////
+  const countGreetings = function(req, res) {
     var count = 0;
     var user = req.params.user;
     user = convertText(user);
-      for (var i = 0; i < greetedUsers.length; i++) {
-        if (user === greetedUsers[i]) {
-          count++;
-        }
+    for (var i = 0; i < greetedUsers.length; i++) {
+      if (user === greetedUsers[i]) {
+        count++;
       }
-      var thisUser = 'Hello, '+user + ' is greeted for the ' + count +' time(s)'
-      res.render('myusers/countGreetings', {thisUser: thisUser});
-
+    }
+    var thisUser = 'Hello, ' + user + ' is greeted for the ' + count + ' time(s)'
+    res.render('myusers/countGreetings', {
+      thisUser: thisUser
+    });
 
   }
+
+
 
   ////////////////GREETED FUNCTION: RESPONDS WITH THE LIST OF GREETED USERS THAT HAVE LINKS/////
-  const greeted = function(req, res, next){
-    models.Users.find({})
-    .exec(function(err, results){
-      if (err) {
-        return next(err);
-      }else {
-        console.log(results);
-      }
+  const greeted = function(req, res) {
+
+    findCollection(function(err, names){
+      res.render('myusers/index', {
+        myusers: names
+      });
     });
-    res.render('myusers/index',{myusers: uniqueList});
 
   }
-  return{
+const resetCounter = function(req, res){
+  models.Users.remove(user, function(err, results){
+
+  })
+}
+  return {
     greet,
     index,
     greeted,
