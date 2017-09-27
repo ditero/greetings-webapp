9892bdc876334a78dbd20c49509f5bc6f7ebc2b9
ditero <anelesigenu@gmail.com>
2017-09-27 16:38:27 +0200

    Editing the tests

--- test/models.test.js
+++ test/models.test.js
@@ -4,8 +4,17 @@ describe('models should be able to store greeted names', function(){
   it('store names to MonngoDB', function(done){
     var models = Models('mongodb://127.0.0.1:27017/greetDB');
 
-    models.Users.create({name: 'The test users'}, function(err){
-      done(err);
+    models.Users.create({name: 'Test Name'}, function(err){
+      if (err) {
+        if (err.code === 11000) {
+          assert.ok(err, "The name is already stored on DB");
+
+        }done()
+
+      }else {
+        assert.ok(err, "Successfully stored on the DB");
+        done()
+      }
 
     });
   });
